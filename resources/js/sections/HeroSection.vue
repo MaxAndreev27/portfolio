1c34a1c132fc26c2607de747ec69dca7f588c797
User <maxandreev27@gmail.com>
2025-11-18 19:02:03 +0200

    Start HeroSection.vue

---
+++ resources/js/sections/HeroSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section class="main-hero container px-15 py-10">
+        <h1 class="text-4xl font-medium">
+            Main Page PHP twin deploy 13.11.2025
+        </h1>
+    </section>
+
+</template>
+
+<style scoped>
+.main-hero {
+    background-color: var(--main-hero-background);
+    color: var(--primary);
+}
+</style>
