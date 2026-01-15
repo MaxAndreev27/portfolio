cad2d37e27f461533b2dab6fc423f42da94099ac
max <maxandreev27@gmail.com>
2026-01-15 20:51:58 +0200

    Disable site Registration via Fortify

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -47,8 +47,12 @@ class HandleInertiaRequests extends Middleware
             ],
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
             'flash' => [
-                'success' => fn () => $request->session()->get('success'),
-                'error' => fn () => $request->session()->get('error'),
+                'success' => fn() => $request->session()->get('success'),
+                'error' => fn() => $request->session()->get('error'),
+            ],
+            'features' => [
+                'registration' => \Laravel\Fortify\Features::enabled(\Laravel\Fortify\Features::registration()),
+                'twoFactorAuthentication' => \Laravel\Fortify\Features::enabled(\Laravel\Fortify\Features::twoFactorAuthentication()),
             ],
         ];
     }
