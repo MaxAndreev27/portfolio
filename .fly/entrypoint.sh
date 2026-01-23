90166ab7deba75b7f0eb8c6e06fb70a58c9ce276
max <maxandreev27@gmail.com>
2026-01-23 17:30:40 +0200

    add cp /var/www/html/.env.example /var/www/html/.env

--- .fly/entrypoint.sh
+++ .fly/entrypoint.sh
@@ -9,6 +9,11 @@ if [ ! -f "$DB_FILE" ]; then
   mkdir -p /var/www/html/database
   touch "$DB_FILE"
 
+  # .env
+  if [ ! -f /var/www/html/.env ]; then
+      cp /var/www/html/.env.example /var/www/html/.env
+  fi
+
   # Генеруємо ключ Laravel, якщо його ще нема
   php artisan key:generate || true
 
